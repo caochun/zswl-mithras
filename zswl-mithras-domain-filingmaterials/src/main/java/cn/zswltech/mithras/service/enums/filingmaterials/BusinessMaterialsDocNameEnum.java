78186b36bd5f95570c5fbb83bf9f166469779b9d
Chun <caochun@gmail.com>
2026-06-01 03:35:09 +0800

    refactor: move filing material enums to filing domain

--- zswl-mithras-domain-filingmaterials/src/main/java/cn/zswltech/mithras/service/enums/filingmaterials/BusinessMaterialsDocNameEnum.java
+++ zswl-mithras-domain-filingmaterials/src/main/java/cn/zswltech/mithras/service/enums/filingmaterials/BusinessMaterialsDocNameEnum.java
@@ -1,19 +1,18 @@
 package cn.zswltech.mithras.service.enums.filingmaterials;
 
-import cn.zswltech.mithras.service.enums.BusinessModuleEnum;
 /**
  * 资料归档-运营基础资料模板
  * @author lllin
  * @date 2025-12-04
  */
 public enum BusinessMaterialsDocNameEnum {
-    BUSINESS_LESSEE_BASIC("承租人基础资料清单.docx","归档-归档资料模板","基础资料清单", BusinessModuleEnum.FILING_BUSINESS_LESSEE_CLIENT.name()),
-    BUSINESS_ENT_GUARANTOR_BASIC("法人保证人基础资料清单.docx","归档-归档资料模板","基础资料清单",BusinessModuleEnum.FILING_BUSINESS_ENT_CLIENT.name()),
-    BUSINESS_IND_GUARANTOR_BASIC("自然人保证人基础资料清单.docx","归档-归档资料模板","基础资料清单",BusinessModuleEnum.FILING_BUSINESS_IND_CLIENT.name()),
-    BUSINESS_CONTRACT("合同资料清单.docx","归档-归档资料模板","合同资料清单",BusinessModuleEnum.FILING_BUSINESS_PAYMENT.name()),
-    BUSINESS_LEASEHOLD("租赁物资料清单.docx","归档-归档资料模板","租赁物资料清单",BusinessModuleEnum.FILING_BUSINESS_LEASEHOLD.name()),
-    BUSINESS_COLLATERALIZATION("抵质押物资料清单.docx","归档-归档资料模板","抵质押物资料清单",BusinessModuleEnum.FILING_BUSINESS_COLLATERALIZATION.name()),
-    BUSINESS_INNER_OPERATION("内部操作资料清单.docx","归档-归档资料模板","内部操作清单",BusinessModuleEnum.FILING_BUSINESS_INNER_OPERATION.name()),
+    BUSINESS_LESSEE_BASIC("承租人基础资料清单.docx","归档-归档资料模板","基础资料清单", "FILING_BUSINESS_LESSEE_CLIENT"),
+    BUSINESS_ENT_GUARANTOR_BASIC("法人保证人基础资料清单.docx","归档-归档资料模板","基础资料清单","FILING_BUSINESS_ENT_CLIENT"),
+    BUSINESS_IND_GUARANTOR_BASIC("自然人保证人基础资料清单.docx","归档-归档资料模板","基础资料清单","FILING_BUSINESS_IND_CLIENT"),
+    BUSINESS_CONTRACT("合同资料清单.docx","归档-归档资料模板","合同资料清单","FILING_BUSINESS_PAYMENT"),
+    BUSINESS_LEASEHOLD("租赁物资料清单.docx","归档-归档资料模板","租赁物资料清单","FILING_BUSINESS_LEASEHOLD"),
+    BUSINESS_COLLATERALIZATION("抵质押物资料清单.docx","归档-归档资料模板","抵质押物资料清单","FILING_BUSINESS_COLLATERALIZATION"),
+    BUSINESS_INNER_OPERATION("内部操作资料清单.docx","归档-归档资料模板","内部操作清单","FILING_BUSINESS_INNER_OPERATION"),
     APPROVE_SNAPSHOT("审批快照.docx","归档-归档资料模板","审批快照",null),
     AFTER_OFFSITE("租后（非现场）管理资料清单.docx","归档-归档资料模板","租后（非现场）管理资料清单",null),
     AFTER_SITE("租后（现场）管理资料清单.docx","归档-归档资料模板","租后（现场）管理资料清单",null),
