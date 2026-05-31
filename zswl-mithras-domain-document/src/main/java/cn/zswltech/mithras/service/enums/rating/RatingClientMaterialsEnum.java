8425d8fd3645a72f0921a674a397fb555a112524
Chun <caochun@gmail.com>
2026-06-01 06:27:25 +0800

    refactor: move material enums to document domain

--- zswl-mithras-domain-document/src/main/java/cn/zswltech/mithras/service/enums/rating/RatingClientMaterialsEnum.java
+++ zswl-mithras-domain-document/src/main/java/cn/zswltech/mithras/service/enums/rating/RatingClientMaterialsEnum.java
@@ -2,7 +2,6 @@ package cn.zswltech.mithras.service.enums.rating;
 
 import cn.zswltech.mithras.service.config.enumscan.IMaterialsTypeConvert;
 import cn.zswltech.mithras.service.config.enumscan.PullDown;
-import cn.zswltech.mithras.service.enums.trackEvent.TrackEventMaterialsEnum;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
