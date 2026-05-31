7835aef70723b530790de0043167efd9152a8789
Chun <caochun@gmail.com>
2026-06-01 04:21:53 +0800

    refactor: move remaining business root enums

--- zswl-mithras-domain-margin/src/main/java/cn/zswltech/mithras/service/enums/MarginWriteOffStatusEnum.java
+++ zswl-mithras-domain-margin/src/main/java/cn/zswltech/mithras/service/enums/MarginWriteOffStatusEnum.java
@@ -1,7 +1,6 @@
 package cn.zswltech.mithras.service.enums;
 
 import cn.zswltech.mithras.service.config.enumscan.PullDown;
-import cn.zswltech.mithras.service.enums.margin.RecordTypeEnum;
 
 /**
  * @create: 2022-08-18
