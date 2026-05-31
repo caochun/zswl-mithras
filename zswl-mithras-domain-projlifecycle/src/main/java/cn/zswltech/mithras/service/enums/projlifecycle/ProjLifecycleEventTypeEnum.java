e21ae4461f1c2b8274eebdc38421c23c05dbe8fa
Chun <caochun@gmail.com>
2026-05-31 21:00:23 +0800

    refactor: extract project lifecycle domain module

--- zswl-mithras-domain-projlifecycle/src/main/java/cn/zswltech/mithras/service/enums/projlifecycle/ProjLifecycleEventTypeEnum.java
+++ zswl-mithras-domain-projlifecycle/src/main/java/cn/zswltech/mithras/service/enums/projlifecycle/ProjLifecycleEventTypeEnum.java
@@ -1,7 +1,6 @@
 package cn.zswltech.mithras.service.enums.projlifecycle;
 
 import cn.zswltech.mithras.service.config.enumscan.PullDown;
-import cn.zswltech.mithras.service.enums.contract.ContractStatus;
 
 /**
  * @create: 2022-10-26
