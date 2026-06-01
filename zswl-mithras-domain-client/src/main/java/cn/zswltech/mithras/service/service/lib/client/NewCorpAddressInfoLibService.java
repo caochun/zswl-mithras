6cf301f7e0038ec0320cef8d071da38e105053b0
Chun <caochun@gmail.com>
2026-06-01 17:38:15 +0800

    refactor: move client address info lib service

---
+++ zswl-mithras-domain-client/src/main/java/cn/zswltech/mithras/service/service/lib/client/NewCorpAddressInfoLibService.java
@@ -0,0 +1,13 @@
+package cn.zswltech.mithras.service.service.lib.client;
+
+import cn.zswltech.mithras.service.mapper.model.client.NewCorpAddressInfoLib;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ *
+ * @author wangchuanhao
+ * @date 2022/6/22 4:11 PM
+ */
+public interface NewCorpAddressInfoLibService extends IService<NewCorpAddressInfoLib> {
+
+}
