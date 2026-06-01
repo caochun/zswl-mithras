e8a45ace11467f7fcddcfee19c44bb5674a09323
Chun <caochun@gmail.com>
2026-06-01 08:18:16 +0800

    refactor: move fund support services to domain module

--- zswl-mithras-domain-fund/src/main/java/cn/zswltech/mithras/service/service/fund/financing/FundLprAdjustRecordService.java
+++ zswl-mithras-domain-fund/src/main/java/cn/zswltech/mithras/service/service/fund/financing/FundLprAdjustRecordService.java
@@ -1,8 +1,6 @@
 package cn.zswltech.mithras.service.service.fund.financing;
 
-import cn.zswltech.mithras.service.mapper.CreditLimitChangeRecordMapper;
 import cn.zswltech.mithras.service.mapper.fund.financing.FundLprAdjustRecordMapper;
-import cn.zswltech.mithras.service.mapper.model.CreditLimitChangeRecord;
 import cn.zswltech.mithras.service.mapper.model.fund.financing.FundLprAdjustRecord;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import lombok.extern.slf4j.Slf4j;
