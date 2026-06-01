27a2c5e06658d3723832dd63037105cc4a5c528a
Chun <caochun@gmail.com>
2026-06-01 13:03:07 +0800

    refactor: move client transfer weight service

---
+++ zswl-mithras-domain-client/src/main/java/cn/zswltech/mithras/service/service/client/ClientTransferWeightService.java
@@ -0,0 +1,17 @@
+package cn.zswltech.mithras.service.service.client;
+
+import cn.zswltech.mithras.service.mapper.client.ClientTransferWeightMapper;
+import cn.zswltech.mithras.service.mapper.model.client.ClientTransferWeight;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author zhaozhengkang
+ * @description
+ * @since
+ */
+@Service
+public class ClientTransferWeightService extends ServiceImpl<ClientTransferWeightMapper, ClientTransferWeight> {
+
+
+}
