c4d81ebf4145182febd3679ae920168aa2717af0
Chun <caochun@gmail.com>
2026-06-03 01:10:52 +0800

    refactor: organize client info history package

---
+++ zswl-mithras-domain-client/src/main/java/cn/zswltech/mithras/client/infohistory/infrastructure/mapper/InfoHistoryMapper.java
@@ -0,0 +1,10 @@
+package cn.zswltech.mithras.client.infohistory.infrastructure.mapper;
+
+import cn.zswltech.mithras.client.infohistory.infrastructure.model.InfoHistory;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * @author luyi
+ */
+public interface InfoHistoryMapper extends BaseMapper<InfoHistory> {
+}
