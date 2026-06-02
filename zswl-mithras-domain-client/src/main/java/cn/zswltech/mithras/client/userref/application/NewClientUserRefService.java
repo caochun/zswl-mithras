896d3b9057468f06fae8a4e6d40528ad3983f1bb
Chun <caochun@gmail.com>
2026-06-03 01:36:06 +0800

    refactor: organize client user ref package

---
+++ zswl-mithras-domain-client/src/main/java/cn/zswltech/mithras/client/userref/application/NewClientUserRefService.java
@@ -0,0 +1,30 @@
+package cn.zswltech.mithras.client.userref.application;
+
+import cn.zswltech.mithras.client.userref.infrastructure.mapper.NewClientUserRefMapper;
+import cn.zswltech.mithras.client.userref.infrastructure.model.NewClientUserRef;
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.core.toolkit.Wrappers;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author dingqi
+ * @date 2024/9/11
+ * @description
+ */
+@Service
+public class NewClientUserRefService extends ServiceImpl<NewClientUserRefMapper, NewClientUserRef> {
+    public void create(Long clientId, Long userId) {
+        NewClientUserRef newClientUserRef = new NewClientUserRef();
+        newClientUserRef.setClientId(clientId);
+        newClientUserRef.setUserId(userId);
+        this.save(newClientUserRef);
+    }
+
+    public int countByClientUser(Long clientId, Long userId) {
+        LambdaQueryWrapper<NewClientUserRef> query = Wrappers.lambdaQuery();
+        query.eq(NewClientUserRef::getClientId, clientId);
+        query.eq(NewClientUserRef::getUserId, userId);
+        return this.count(query);
+    }
+}
---
+++ zswl-mithras-domain-client/src/main/java/cn/zswltech/mithras/client/userref/infrastructure/model/NewClientUserRef.java
@@ -0,0 +1,29 @@
+package cn.zswltech.mithras.client.userref.infrastructure.model;
+
+import cn.zswltech.mithras.service.mapper.model.BaseModel;
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+/**
+ * @author dingqi
+ * @date 2024/9/11
+ * @description
+ */
+@EqualsAndHashCode(callSuper = true)
+@Data
+@TableName("new_client_user_ref")
+public class NewClientUserRef extends BaseModel {
+    @TableId(type = IdType.AUTO)
+    @TableField(value = "id")
+    private Long id;
+
+    @TableField(value = "client_id")
+    private Long clientId;
+
+    @TableField(value = "user_id")
+    private Long userId;
+}
