c69c4a90f646a0708f4d67b622f8fbe5da414859
Chun <caochun@gmail.com>
2026-06-04 21:09:50 +0800

    refactor: move new financing cost metric calculator

---
+++ zswl-mithras-metric/src/main/java/cn/zswltech/mithras/metric/financialcloudmetric/calculator/FtpAssessmentInfoReader.java
@@ -0,0 +1,27 @@
+package cn.zswltech.mithras.metric.financialcloudmetric.calculator;
+
+import cn.zswltech.mithras.payment.infrastructure.persistence.mapper.FtpAssessmentInfoMapper;
+import cn.zswltech.mithras.payment.infrastructure.persistence.mapper.model.FtpAssessmentInfo;
+import cn.zswltech.mithras.service.enums.YesOrNoNumberEnum;
+import com.baomidou.mybatisplus.core.toolkit.Wrappers;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.Resource;
+import java.time.LocalDate;
+
+@Component
+public class FtpAssessmentInfoReader {
+
+    @Resource
+    private FtpAssessmentInfoMapper ftpAssessmentInfoMapper;
+
+    public FtpAssessmentInfo findLatestEffect(LocalDate targetDate, Long receiptId) {
+        return ftpAssessmentInfoMapper.selectOne(Wrappers.<FtpAssessmentInfo>lambdaQuery()
+                .eq(FtpAssessmentInfo::getReceiptId, receiptId)
+                .eq(FtpAssessmentInfo::getIsEffect, YesOrNoNumberEnum.YES.getCode())
+                .le(FtpAssessmentInfo::getEffectDate, targetDate)
+                .orderByDesc(FtpAssessmentInfo::getEffectDate)
+                .orderByDesc(FtpAssessmentInfo::getId)
+                .last("LIMIT 1"));
+    }
+}
