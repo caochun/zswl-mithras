71820c95ecb150420b2e4ef88128949a5475c405
Chun <caochun@gmail.com>
2026-06-04 18:01:35 +0800

    refactor: move monthly excel models

--- zswl-mithras-monthly/src/main/java/cn/zswltech/mithras/service/excel/model/monthly/MonthlyDutyProjExcelModel.java
+++ zswl-mithras-monthly/src/main/java/cn/zswltech/mithras/service/excel/model/monthly/MonthlyDutyProjExcelModel.java
@@ -1,9 +1,7 @@
 package cn.zswltech.mithras.service.excel.model.monthly;
 
 import cn.zswltech.mithras.service.excel.annotation.SimpleExcelHeader;
-import cn.zswltech.mithras.service.excel.model.CashFlowExcelModel;
 import cn.zswltech.mithras.service.excel.model.ExcelModel;
-import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
