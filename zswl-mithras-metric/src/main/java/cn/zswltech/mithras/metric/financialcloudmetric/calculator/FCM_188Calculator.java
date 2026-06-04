6956b89bcf9a979bf9b70c68b66fdf8c0b4a4024
Chun <caochun@gmail.com>
2026-06-04 20:10:24 +0800

    refactor: move department per capital calculators

---
+++ zswl-mithras-metric/src/main/java/cn/zswltech/mithras/metric/financialcloudmetric/calculator/FCM_188Calculator.java
@@ -0,0 +1,51 @@
+package cn.zswltech.mithras.metric.financialcloudmetric.calculator;
+
+import cn.zswltech.mithras.metric.financialcloudmetric.calculator.enums.Department;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.time.LocalDate;
+
+/**
+ * @description:1
+ * @author: zhaozhengkang
+ * @date: 2023/4/13 09:57
+ */
+@Slf4j
+@Component
+public class FCM_188Calculator extends DepartmentPerCapitalCalculator {
+
+
+    @Override
+    public String metricCode() {
+        return "FCM_188";
+    }
+
+    @Override
+    protected String departmentCode() {
+        return Department.HYYWB.name();
+    }
+
+    @Override
+    protected String totalMetricCode() {
+        return "FCM_184";
+    }
+
+    @Override
+    public BigDecimal calculate(LocalDate dateTime) {
+        return super.calculate(dateTime);
+    }
+
+    @Override
+    protected BigDecimal doCal(int personCount) {
+        log.info("部门：{}，部门人数：{}", Department.valueOf(departmentCode()).getName(), personCount);
+        BigDecimal bigDecimal = STOCK_INVESTMENT_BALANCE.get(departmentCode());
+        if (bigDecimal != null) {
+            return bigDecimal.divide(new BigDecimal(personCount), 2, RoundingMode.HALF_UP);
+        }
+        return BigDecimal.ZERO;
+    }
+
+}
