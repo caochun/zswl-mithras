3e63091ace55623027f7ed476e43c8ecd2ad0d05
Chun <caochun@gmail.com>
2026-06-04 20:30:04 +0800

    refactor: move asset return ratio metric calculator

---
+++ zswl-mithras-metric/src/main/java/cn/zswltech/mithras/metric/financialcloudmetric/calculator/FCM_211Calculator.java
@@ -0,0 +1,34 @@
+package cn.zswltech.mithras.metric.financialcloudmetric.calculator;
+
+import cn.zswltech.mithras.metric.financialcloudmetric.model.FinancialCloudMetricValue;
+import cn.zswltech.mithras.metric.financialcloudmetric.service.FinancialCloudMetricValueReader;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.Resource;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.temporal.TemporalAdjusters;
+
+/**
+ * @description: FCM_179/FCM_207 1
+ * @author: zhaozhengkang
+ * @date: 2023/4/13 09:59
+ */
+@Component
+public class FCM_211Calculator extends TwoMetricCalculator {
+
+    @Resource
+    private FinancialCloudMetricValueReader cloudMetricValueReader;
+
+    @Override
+    public String metricCode() {
+        return "FCM_211";
+    }
+
+    @Override
+    public BigDecimal calculate(LocalDate dateTime) {
+        FinancialCloudMetricValue fcm179 = cloudMetricValueReader.getMetricValue("FCM_179", dateTime);
+        FinancialCloudMetricValue fcm207 = cloudMetricValueReader.getMetricValue("FCM_207", dateTime.with(TemporalAdjusters.firstDayOfYear()));
+        return division(fcm179, fcm207, 4);
+    }
+}
