4b42e935789af1fa9a0e874d70d4cf8fb5497609
Chun <caochun@gmail.com>
2026-06-05 14:55:44 +0800

    refactor: move projectprocess factoring price handlers

---
+++ zswl-mithras-projectprocess/src/main/java/cn/zswltech/mithras/projectprocess/service/ProjectProcessSurvivingContractResolver.java
@@ -0,0 +1,17 @@
+package cn.zswltech.mithras.projectprocess.service;
+
+import cn.zswltech.mithras.api.contract.ContractBaseInfoRSP;
+
+import java.util.List;
+
+/**
+ * Contract lookup boundary used by project-process factoring price views.
+ */
+public interface ProjectProcessSurvivingContractResolver {
+
+    List<ContractBaseInfoRSP> resolveProjEstablishSurvivingContracts(Long projEstablishId);
+
+    List<ContractBaseInfoRSP> resolveProjPricingSurvivingContracts(Long projectId);
+
+    List<ContractBaseInfoRSP> resolveProjReviewSurvivingContracts(Long projectId);
+}
