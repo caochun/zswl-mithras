d09369bc5e95e424d7d0722efc7a2666e07f1859
Chun <caochun@gmail.com>
2026-06-05 10:09:03 +0800

    refactor: move cq sql resources into domain modules

---
+++ zswl-mithras-margin/src/main/resources/sql/cq/20221214.sql
@@ -0,0 +1,3 @@
+
+ALTER TABLE margin_record_info ADD rent_collection_code varchar(50) NULL COMMENT '租金现金流编号';
+
--- zswl-mithras-service/src/main/resources/sql/cq/20221214.sql
+++
@@ -1,3 +0,0 @@
-
-ALTER TABLE margin_record_info ADD rent_collection_code varchar(50) NULL COMMENT '租金现金流编号';
-
