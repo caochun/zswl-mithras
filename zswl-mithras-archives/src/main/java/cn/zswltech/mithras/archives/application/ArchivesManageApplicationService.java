c060456cdd6847e555115e9c407fd20f13618cc0
Chun <caochun@gmail.com>
2026-06-06 01:20:06 +0800

    refactor: move archives and basedata controllers

---
+++ zswl-mithras-archives/src/main/java/cn/zswltech/mithras/archives/application/ArchivesManageApplicationService.java
@@ -0,0 +1,6 @@
+package cn.zswltech.mithras.archives.application;
+
+import cn.zswltech.mithras.api.archives.ArchivesManageApi;
+
+public interface ArchivesManageApplicationService extends ArchivesManageApi {
+}
---
+++ zswl-mithras-basedata/src/main/java/cn/zswltech/mithras/basedata/application/BaseDataBankAccountApplicationService.java
@@ -0,0 +1,6 @@
+package cn.zswltech.mithras.basedata.application;
+
+import cn.zswltech.mithras.api.basedata.BaseDataBankAccountApi;
+
+public interface BaseDataBankAccountApplicationService extends BaseDataBankAccountApi {
+}
