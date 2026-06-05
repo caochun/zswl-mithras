9f53759fdbb33ceacb6981765c01b2c6f8d4a57b
Chun <caochun@gmail.com>
2026-06-05 18:55:23 +0800

    refactor: move workflow track event excel support to domain

--- zswl-mithras-workflow/src/main/java/cn/zswltech/mithras/workflow/excel/exporter/TrackEventExcelManagerExporter.java
+++ zswl-mithras-workflow/src/main/java/cn/zswltech/mithras/workflow/excel/exporter/TrackEventExcelManagerExporter.java
@@ -1,7 +1,7 @@
-package cn.zswltech.mithras.service.excel.exporter;
+package cn.zswltech.mithras.workflow.excel.exporter;
 
 import cn.zswltech.mithras.service.excel.AbstractSimpleExcelExporter;
-import cn.zswltech.mithras.service.excel.model.TrackEventExcelExporter;
+import cn.zswltech.mithras.workflow.excel.model.TrackEventExcelExporter;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.springframework.stereotype.Component;
 
--- zswl-mithras-workflow/src/main/java/cn/zswltech/mithras/workflow/excel/model/TrackEventExcelExporter.java
+++ zswl-mithras-workflow/src/main/java/cn/zswltech/mithras/workflow/excel/model/TrackEventExcelExporter.java
@@ -1,6 +1,7 @@
-package cn.zswltech.mithras.service.excel.model;
+package cn.zswltech.mithras.workflow.excel.model;
 
 import cn.zswltech.mithras.service.excel.annotation.SimpleExcelHeader;
+import cn.zswltech.mithras.service.excel.model.ExcelModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 import lombok.experimental.Accessors;
