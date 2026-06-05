63db74b93876e7154af2d617a692ab7b0f339a59
Chun <caochun@gmail.com>
2026-06-05 21:29:37 +0800

    refactor: move credit search project controller

--- zswl-mithras-creditreport/src/main/java/cn/zswltech/mithras/creditreport/controller/CreditSearchProjectController.java
+++ zswl-mithras-creditreport/src/main/java/cn/zswltech/mithras/creditreport/controller/CreditSearchProjectController.java
@@ -7,7 +7,7 @@ import cn.zswltech.mithras.dto.creditreport.CreditReportListDTO;
 import cn.zswltech.mithras.dto.creditreport.CreditReportProjectReviewAddDTO;
 import cn.zswltech.mithras.dto.creditreport.CreditSearchProjectCmd;
 import cn.zswltech.mithras.dto.creditreport.CreditSearchProjectQuery;
-import cn.zswltech.mithras.creditreport.service.CreditReportBaseInfoService;
+import cn.zswltech.mithras.creditreport.service.CreditReportQueryService;
 import cn.zswltech.mithras.creditreport.service.CreditSearchProjectService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,7 +21,7 @@ public class CreditSearchProjectController implements CreditSearchProjectApi {
     @Resource
     private CreditSearchProjectService creditSearchProjectService;
     @Resource
-    private CreditReportBaseInfoService creditReportBaseInfoService;
+    private CreditReportQueryService creditReportQueryService;
 
     @Override
     public R<CreditReportProjectReviewAddDTO> showCreditReportByProjId(CreditSearchProjectCmd cmd) {
@@ -36,7 +36,7 @@ public class CreditSearchProjectController implements CreditSearchProjectApi {
     @Override
     public R<Void> delete(Long id) {
         //creditSearchProjectService.delete(id);
-        creditReportBaseInfoService.remove(id);
+        creditReportQueryService.remove(id);
         return R.ok();
     }
 }
