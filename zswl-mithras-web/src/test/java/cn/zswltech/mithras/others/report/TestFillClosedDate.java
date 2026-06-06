070e6563a74f353b1c2d5f0ca6a1fa33f2370d6c
Chun <caochun@gmail.com>
2026-06-06 10:35:58 +0800

    refactor: move finance auto writeoff job to capital

--- zswl-mithras-web/src/test/java/cn/zswltech/mithras/others/report/TestFillClosedDate.java
+++ zswl-mithras-web/src/test/java/cn/zswltech/mithras/others/report/TestFillClosedDate.java
@@ -8,6 +8,7 @@ import cn.zswltech.mithras.dto.dashboard.DashboardFundFinanceBalanceRSP;
 import cn.zswltech.mithras.dto.dashboard.DashboardFundFinanceLoanInfoREQ;
 import cn.zswltech.mithras.dto.dashboard.DashboardFundFinanceLoanInfoRSP;
 import cn.zswltech.mithras.metric.job.MonthlyTaskGenerator;
+import cn.zswltech.mithras.capital.job.FinanceAutoWriteOffJob;
 import cn.zswltech.mithras.report.enums.biz.DataTypeEnum;
 import cn.zswltech.mithras.report.enums.common.ApprovalStatus;
 import cn.zswltech.mithras.report.enums.common.ReportState;
