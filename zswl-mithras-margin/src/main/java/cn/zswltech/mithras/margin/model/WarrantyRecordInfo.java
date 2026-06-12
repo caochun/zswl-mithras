8d4b5c8e9b070eed061ed3068848fd24333344bf
Chun <caochun@gmail.com>
2026-06-12 08:53:17 +0800

    refactor: tidy margin imports

--- zswl-mithras-margin/src/main/java/cn/zswltech/mithras/margin/model/WarrantyRecordInfo.java
+++ zswl-mithras-margin/src/main/java/cn/zswltech/mithras/margin/model/WarrantyRecordInfo.java
@@ -1,7 +1,11 @@
 package cn.zswltech.mithras.margin.model;
 
 import cn.zswltech.mithras.foundation.persistence.model.BaseModelWithLogicDelete;
-import com.baomidou.mybatisplus.annotation.*;
+import com.baomidou.mybatisplus.annotation.FieldStrategy;
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
