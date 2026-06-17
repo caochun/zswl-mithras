ccdb3a3517bc681991c2644f093402d5777cf8e3
Chun <caochun@gmail.com>
2026-06-18 01:13:13 +0800

    refactor: share dashboard operation view columns

--- zswl-mithras-react/src/pages/dashboard/workbench/OperationView/LaunchComplete/ListDrawer/index.js
+++ zswl-mithras-react/src/pages/dashboard/workbench/OperationView/LaunchComplete/ListDrawer/index.js
@@ -5,7 +5,7 @@ import { ALL_COLUMNS } from './Column'
 import { operationViewColumnsFilterKey as columnsFilterKey } from '@/utils/dashboardFilterKeys'
 import { ExportBtn, TableSummary } from '@/components/Dashboard'
 import { initQueryDate } from '@/utils/dashboardOperation'
-import { COMMON_COLUMNS } from '@/pages/dashboard/workbench/OperationView/Column'
+import { COMMON_COLUMNS } from '@/components/Dashboard/OperationViewColumns'
 import { saveServer } from '@/utils'
 
 const groupName = '投放完成情况详情'
