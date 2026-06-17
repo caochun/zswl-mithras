45a1f030c8d06dede3f6e5694b908c879e9b67ca
Chun <caochun@gmail.com>
2026-06-18 04:33:06 +0800

    refactor: share credit search list

--- zswl-mithras-react/src/components/Credit/CreditSearchModal/index.js
+++ zswl-mithras-react/src/components/Credit/CreditSearchModal/index.js
@@ -1,6 +1,6 @@
 import { Button, Drawer } from '@zswl/components'
 import { observer } from '@zswl/admin'
-import SearchList from '@/pages/creditManage/search'
+import SearchList from '@/components/Credit/CreditSearchList'
 import { useState, cloneElement } from 'react'
 
 const CreditSearchModal = ({
