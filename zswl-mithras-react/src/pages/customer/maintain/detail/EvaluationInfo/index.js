796ca8b3f05f76b823bb5873d76c9000580969ca
Chun <caochun@gmail.com>
2026-06-18 01:33:06 +0800

    refactor: share customer rating columns

--- zswl-mithras-react/src/pages/customer/maintain/detail/EvaluationInfo/index.js
+++ zswl-mithras-react/src/pages/customer/maintain/detail/EvaluationInfo/index.js
@@ -2,7 +2,7 @@ import { Button, Page, Table } from '@zswl/components'
 import { observer } from '@zswl/admin'
 import { useMemo } from 'react'
 import { getTableColumns } from '@/utils'
-import ALL_COLUMNS from '@/pages/customer/customerRat/Columns.js'
+import ALL_COLUMNS from '@/components/Customer/CustomerRatColumns'
 import { saveServer } from '@/utils'
 
 function Index({ path, store }) {
