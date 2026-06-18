289db6fad9aee64ad2260bb69d3f12ac6535d331
Chun <caochun@gmail.com>
2026-06-18 09:48:26 +0800

    refactor: remove unused component compatibility shims

--- zswl-mithras-react/src/components/BlackGray/BlackInfo/index.js
+++
@@ -1,1 +0,0 @@
-export { default, BlackInfo } from '@/components/BlackInfo'
--- zswl-mithras-react/src/components/Contract/PaymentApply/index.js
+++ zswl-mithras-react/src/components/Contract/PaymentApply/index.js
@@ -1,5 +1,5 @@
 import EditDescription from '@/components/Table/EditDescription'
-import ALL_COLUMNS from './Column'
+import ALL_COLUMNS from '@/components/PaymentApplyColumns'
 import { getDescColumns } from '@/utils'
 import { useEffect, useMemo, useState } from 'react'
 import Api from './api'
