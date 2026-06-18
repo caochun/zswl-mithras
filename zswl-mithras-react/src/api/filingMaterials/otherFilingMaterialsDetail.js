ea8b4badc024fae9867be8aeeeab5cb9f60953b8
Chun <caochun@gmail.com>
2026-06-19 00:52:16 +0800

    refactor: host filing materials api under corrected domain

--- zswl-mithras-react/src/api/filingMaterials/otherFilingMaterialsDetail.js
+++ zswl-mithras-react/src/api/filingMaterials/otherFilingMaterialsDetail.js
@@ -1,1 +1,28 @@
-export { default } from '../fillingMaterials/otherFilingMaterialsDetail'
+import { http } from '@zswl/admin'
+
+export default {
+  getOperationsDirDict: (id) => http.post('/other/filingMaterial/getOperationsDirDict', { id }),
+  getMaterialsDesc: (id) => http.post('/other/filingMaterial/getMaterialsDesc', { id }),
+  saveMaterialsDesc: (params) =>
+    http.post('/other/filingMaterial/saveMaterialsDesc', params, {
+      transformRequest: [
+        (data) => {
+          return JSON.stringify({ ...data, materialsDesc: data.materialsDesc || '' })
+        },
+      ],
+      headers: { 'Content-Type': 'application/json' },
+    }),
+  checkMaterialsDesc: (params) => http.post('/other/filingMaterial/checkMaterialsDesc', params),
+  remove: (params) =>
+    http.post('/other/filingMaterial/file/remove', params, { transformResult: (res) => res.data }),
+  batchDownload: (params) =>
+    http.post(
+      '/other/filingMaterial/batchDownload',
+      {
+        ...params,
+        type: 'download',
+        timeout: 0,
+      },
+      { type: 'download' }
+    ),
+}
