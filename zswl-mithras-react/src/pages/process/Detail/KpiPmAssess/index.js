a0cf34d1e695da1d98cb3170cee7f791f07bdaa1
Chun <caochun@gmail.com>
2026-06-18 10:42:18 +0800

    refactor: expose kpi pm assess detail content

--- zswl-mithras-react/src/pages/process/Detail/KpiPmAssess/index.js
+++ zswl-mithras-react/src/pages/process/Detail/KpiPmAssess/index.js
@@ -1,6 +1,6 @@
 import { useMemo } from 'react'
 import { observer } from '@zswl/admin'
-import PmAssess from '@/components/Kpi/PmAssess/EditModal/Content'
+import PmAssess from '@/components/Kpi/PmAssess/DetailContent'
 
 const Index = (props) => {
   const { canEditFlag } = props
