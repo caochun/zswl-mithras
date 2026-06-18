793c8d6f3f0c54c1c1c987f374e539b4bb000947
Chun <caochun@gmail.com>
2026-06-18 10:27:45 +0800

    refactor: move policy component out of cpm

--- zswl-mithras-react/src/pages/dashboard/workbench/UnifiedToDo/MyPolicy/CreateDraw.js
+++ zswl-mithras-react/src/pages/dashboard/workbench/UnifiedToDo/MyPolicy/CreateDraw.js
@@ -1,6 +1,6 @@
 import { observer } from '@zswl/admin'
 import { Drawer, Button } from '@zswl/components'
-import Policy from '@/components/Cpm/Policy'
+import Policy from '@/components/Policy'
 import { Space } from 'antd'
 
 const Index = ({ store }) => {
