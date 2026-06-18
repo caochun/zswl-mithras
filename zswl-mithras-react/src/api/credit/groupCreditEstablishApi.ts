f043fd63c5e5857b357d32c0662fe6d656ef0ae3
Chun <caochun@gmail.com>
2026-06-19 00:24:49 +0800

    refactor: host group credit api under credit domain

--- zswl-mithras-react/src/api/credit/groupCreditEstablishApi.ts
+++ zswl-mithras-react/src/api/credit/groupCreditEstablishApi.ts
@@ -1,1 +1,32 @@
-export { default } from '@/api/groupCredit/projectApprovalBaseinfo'
+/* prettier-ignore-start */
+import * as Types from '@/api/groupCredit/interface/projectApprovalBaseinfo'
+import { http } from '@zswl/admin'
+
+const mock = false
+// const mock= { delay: 800 }
+export default {
+  // 修改集团授信立项基本信息
+  postInfoModify: (data: Types.InfoModifyRequest): Promise<Types.InfoModifyResponse> =>
+    http.post('/group/credit/establish/base/info/modify', data, { mock }),
+
+  // 新增集团授信立项基本信息
+  postInfoAdd: (data: Types.InfoAddRequest): Promise<Types.InfoAddResponse> =>
+    http.post('/group/credit/establish/base/info/add', data, { mock }),
+
+  // 获取集团授信存量风险敞口
+  postCreditExposure: (data: Types.CreditExposureRequest): Promise<Types.CreditExposureResponse> =>
+    http.post('/group/credit/exposure', data, { mock }),
+
+  // 集团授信立项基本信息列表
+  postInfoList: (data: Types.InfoListRequest): Promise<Types.InfoListResponse> =>
+    http.post('/group/credit/establish/base/info/list', data, { mock }),
+
+  // 集团授信立项基本信息详情
+  postInfoDetail: (data: Types.InfoDetailRequest): Promise<Types.InfoDetailResponse> =>
+    http.post('/group/credit/establish/base/info/detail', data, { mock }),
+  // 集团授信立项基本信息对比
+  postInfoDetailCompare: (data: Types.InfoDetailRequest): Promise<any> =>
+    http.post('/group/credit/establish/base/info/detail/compare', data, { mock }),
+}
+
+/* prettier-ignore-end */
--- zswl-mithras-react/src/api/credit/groupCreditEstablishVersionApi.ts
+++ zswl-mithras-react/src/api/credit/groupCreditEstablishVersionApi.ts
@@ -1,1 +1,25 @@
-export { default } from '@/api/groupCredit/projectApprovalVersion'
+/* prettier-ignore-start */
+import * as Types from '@/api/groupCredit/interface/projectApprovalVersion'
+import { http } from '@zswl/admin'
+
+const mock = false
+// const mock= { delay: 800 }
+export default {
+  // 集团授信立项信息版本比较详情（与上一版本比较）
+  postComparePreVersion: (
+    data: Types.ComparePreVersionRequest,
+  ): Promise<Types.ComparePreVersionResponse> =>
+    http.post('/group/credit/establish/compare/preVersion', data, { mock }),
+
+  // 集团授信立项信息版本表列
+  postVersionList: (data: Types.VersionListRequest): Promise<Types.VersionListResponse> =>
+    http.post('/group/credit/establish/version/list', data, { mock }),
+
+  // 集团授信立项信息生效（或提交审批）
+  postEstablishEffect: (
+    data: Types.EstablishEffectRequest,
+  ): Promise<Types.EstablishEffectResponse> =>
+    http.post('/group/credit/establish/effect', data, { mock }),
+}
+
+/* prettier-ignore-end */
