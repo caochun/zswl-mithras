eed65c44369d9a6917fd9fe1799fd1b8863e2feb
Chun <caochun@gmail.com>
2026-06-19 00:29:18 +0800

    refactor: host legacy api implementations in semantic domains

--- zswl-mithras-react/src/api/budget/pricing/ftpInterest.ts
+++ zswl-mithras-react/src/api/budget/pricing/ftpInterest.ts
@@ -1,1 +1,22 @@
-export { default } from '../../pricing/ftpInterest'
+/* prettier-ignore-start */
+import * as Types from '@/api/budget/pricing/interface/ftpInterest'
+import { http } from '@zswl/admin'
+
+const mock = false
+// const mock = { mode: 2 }
+export default {
+  // FTP计息-基本信息
+  postBaseinfoGet: (data: Types.BaseinfoGetRequest): Promise<Types.BaseinfoGetResponse> =>
+    http.post('/ftp/interest/baseinfo/get', data, { mock }),
+
+  // FTP计息-每日计息分页列表
+  postDetailPagelist: (data: Types.DetailPagelistRequest): Promise<Types.DetailPagelistResponse> =>
+    http.post('/ftp/interest/detail/pagelist', data, { mock }),
+
+  // FTP计息分页列表
+  postInterestPagelist: (
+    data: Types.InterestPagelistRequest
+  ): Promise<Types.InterestPagelistResponse> => http.post('/ftp/interest/pagelist', data, { mock }),
+}
+
+/* prettier-ignore-end */
