6eeeb4eafa14e90fb26e75d4a274d3e51aa449e7
Chun <caochun@gmail.com>
2026-06-19 00:19:54 +0800

    refactor: move report kpi and rzy utilities to domain entries

--- zswl-mithras-react/src/utils/domains/kpi/KpiUtils.js
+++ zswl-mithras-react/src/utils/domains/kpi/KpiUtils.js
@@ -1,1 +1,52 @@
-export * from '@/utils/kpi'
+import { hasValue } from '@/utils/base'
+
+export const formulaData = (value) => {
+  if (hasValue(value) && String(value).indexOf('=') === 0) {
+    return {
+      value: value.slice(1),
+      configValueType: 'FORMULA',
+    }
+  }
+  return {
+    value,
+    configValueType: 'VALUE',
+  }
+}
+
+// 后端维护 kpiProjectWeightTypeEnum
+export const AllocateEnum = [
+  { label: '业务部门', value: 'BUSINESS_DEPT' },
+  { label: '项目主办', value: 'PROJECT_SPONSOR' },
+  { label: '项目协办', value: 'PROJECT_COSPONSOR' },
+  { label: '跨部门推荐人', value: 'OTHER_DEPT_RECOMMEND' },
+]
+
+export const TagColor = {
+  BUSINESS_DEPT: '#2db7f5',
+  PROJECT_SPONSOR: '#f50',
+  PROJECT_COSPONSOR: '#108ee9',
+  OTHER_DEPT_RECOMMEND: '#87d068',
+}
+
+export const AllocateTypeInfo = {
+  BUSINESS_DEPT: {
+    allocateLabel: '业务部门',
+    allocatePlaceHold: '业务部门',
+    ratioLable: '部门池',
+  },
+  PROJECT_SPONSOR: {
+    allocateLabel: '项目主办',
+    allocatePlaceHold: '项目经理',
+    ratioLable: '主办占比',
+  },
+  PROJECT_COSPONSOR: {
+    allocateLabel: '项目协办',
+    allocatePlaceHold: '项目经理',
+    ratioLable: '协办占比',
+  },
+  OTHER_DEPT_RECOMMEND: {
+    allocateLabel: '跨部门推荐人',
+    allocatePlaceHold: '项目经理',
+    ratioLable: '推荐人占比',
+  },
+}
