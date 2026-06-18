fed93734766b43701a55ebfc9b737d74802b6630
Chun <caochun@gmail.com>
2026-06-18 09:26:02 +0800

    refactor: remove unused project establishment shims

--- zswl-mithras-react/src/pages/project/establishment/detail/BaseInfo/FormConfig/utils.js
+++
@@ -1,1 +0,0 @@
-export * from '@/components/Project/EstablishmentDetail/BaseInfo/FormConfig/utils'
--- zswl-mithras-react/src/pages/project/establishment/detail/QuotationScheme/FormConfig/utils.js
+++
@@ -1,1 +0,0 @@
-export * from '@/components/Project/EstablishmentDetail/QuotationScheme/FormConfig/utils'
