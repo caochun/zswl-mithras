98a3f8003f50de3037dd665fb7d6d399bc188133
Chun <caochun@gmail.com>
2026-06-19 18:53:39 +0800

    refactor: move credit establish list into domain

--- zswl-mithras-react/src/components/Credit/EstablishEntries.js
+++ zswl-mithras-react/src/components/Credit/EstablishEntries.js
@@ -1,2 +1,3 @@
+export { default as CreditEstablish } from './Establish'
 export { default as CreditEstablishDetail } from './EstablishDetail'
 export { default as CreditEstablishDetailBaseInfo } from './EstablishDetail/BaseInfo'
