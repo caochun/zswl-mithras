f00c6001b05e5295e1d55692f13292a519f9a731
Chun <caochun@gmail.com>
2026-06-19 15:28:11 +0800

    refactor: move permission log into component domain

--- zswl-mithras-react/src/components/Permission/BifrostEntries.js
+++ zswl-mithras-react/src/components/Permission/BifrostEntries.js
@@ -1,1 +1,2 @@
 export { default as PermissionBifrost } from './Bifrost'
+export { default as PermissionLog } from './Log'
---
+++ zswl-mithras-react/src/components/Permission/Log/index.js
@@ -0,0 +1,41 @@
+import { Page, SearchBar, Table } from '@zswl/components'
+import { observer } from '@zswl/admin'
+import store from './store'
+import { JSONRender } from '@/components/Format'
+import { FounderSelect } from '@/components/Select'
+import { saveServer } from '@/utils'
+
+const { Item } = SearchBar
+function Index({ path }) {
+  const columns = [
+    { title: '接口路径', dataIndex: 'url' },
+    { title: '操作人', dataIndex: 'userName' },
+    { title: '接口参数', dataIndex: 'reqData', render: JSONRender },
+    { title: '操作时间', dataIndex: 'operateTime' },
+  ]
+
+  return (
+    <Page>
+      <Table
+        columnsFilter={'permission_log_1'}
+        onFilter={(key, val) => saveServer('permission_log_1', val)}
+        store={store.table}
+        editable={false}
+        selectable
+        scroll={{ x: 'auto' }}
+        resizable
+        searchbar={{
+          labelCol: { span: 6 },
+          items: [
+            <Item label="操作人" name="userId">
+              <FounderSelect params={{ job: null }}></FounderSelect>
+            </Item>,
+          ],
+        }}
+        columns={columns}
+      />
+    </Page>
+  )
+}
+
+export default observer(Index)
