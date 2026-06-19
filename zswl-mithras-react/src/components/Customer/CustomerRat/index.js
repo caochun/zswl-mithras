a0e604dd4ecc88ceff412871d2637b8cca27dd97
Chun <caochun@gmail.com>
2026-06-19 18:36:17 +0800

    refactor: move customer rating list into domain

---
+++ zswl-mithras-react/src/components/Customer/CustomerRat/index.js
@@ -0,0 +1,107 @@
+import { Button, Modal, Page, Table } from '@zswl/components'
+import { observer } from '@zswl/admin'
+import store from './store'
+import { getTableColumns } from '@/utils'
+import ALL_COLUMNS from '../CustomerRatColumns'
+import { useMemo } from 'react'
+import CreateModal from './CreateModal'
+import useSearch from '@/utils/hooks/useSearch'
+import { saveServer } from '@/utils'
+
+function Index({ path, query }) {
+  const canDelete = store.table.selectedRowKeys.length > 0
+  const columns = useMemo(() => {
+    const nameColumns = [
+      {
+        title: '客户编号',
+        search: true,
+        width: 140,
+        actions: ({ clientCode: name, pricingFrequency, id, modelCode }) => [
+          {
+            name,
+            to: `${path}/detail/${id}?model=${modelCode}`,
+          },
+        ],
+      },
+      { title: '客户名称', search: true, width: 260 },
+      { title: '模型名称', search: true },
+      '评级结果',
+      '评级认定结果',
+      { title: '发起时间', search: true },
+      { title: '发起机构', width: 140 },
+      '发起人',
+      { title: '流程状态', search: true },
+      { title: '评级状态', search: true },
+      { title: '评级类型', search: true },
+    ]
+
+    return [
+      ...getTableColumns(ALL_COLUMNS, nameColumns, true),
+      {
+        title: '操作',
+        width: 200,
+        actions: (record) => {
+          const disabled = record.ratingStatus === 'true'
+          return [
+            {
+              name: '修改',
+              onClick: () => store.edit(record),
+              disabled,
+            },
+            { name: '查看调整记录', onClick: () => store.view(record.id) },
+            {
+              name: '删除',
+              onClick: () => store.delete(record.id),
+              confirm: '确定删除该评级吗？',
+              disabled,
+              style: { color: disabled ? undefined : 'red' },
+            },
+          ]
+        },
+      },
+    ]
+  }, [])
+  useSearch({ query, store: store.table })
+  return (
+    <Page>
+      <Table
+        columnsFilter={'customer_customerRat_1'}
+        onFilter={(key,val) => saveServer('customer_customerRat_1',val)}
+        store={store.table}
+        editable={false}
+        autoRequest={false}
+        columnWidth={140}
+        actions={[
+          <Button.Add onClick={store.add} key="add">
+            发起评级
+          </Button.Add>,
+        ]}
+        scroll={{ x: 1200 }}
+        columns={columns}
+      />
+      <CreateModal store={store} />
+      <Modal store={store.recordModal} title="调整记录" width={800} footer={[]}>
+        <Table
+                columnsFilter={'customer_customerRat_2'}
+                onFilter={(key,val) => saveServer('customer_customerRat_2',val)}
+          editable={false}
+          autoRequest={false}
+          columnWidth={80}
+          scroll={{ x: 'auto' }}
+          pagination={false}
+          columns={[
+            { title: '调整时间', dataIndex: 'overturnTime' },
+            { title: '系统评级结果', dataIndex: 'score' },
+            { title: '审查结果', dataIndex: 'finalScore' },
+            { title: '调整类型', dataIndex: 'adjustType' },
+            { title: '调整理由', dataIndex: 'overturnOpinion' },
+            { title: '调整人', dataIndex: 'overturnUserName' },
+          ]}
+          store={store.recordTable}
+        />
+      </Modal>
+    </Page>
+  )
+}
+
+export default observer(Index)
---
+++ zswl-mithras-react/src/components/Customer/CustomerRat/store.js
@@ -0,0 +1,130 @@
+import { Modal, ModalStore, TableStore } from '@zswl/components'
+import { history, makeAutoObservable } from '@zswl/admin'
+import customerRatApi from '@/api/customer/customerRat/customerRatApi'
+import { message } from 'antd'
+
+class Store {
+  constructor() {
+    makeAutoObservable(this)
+  }
+  isHymx = false
+  showDistrict = false
+  clientDisabled = true
+  clientCodeRequire = true
+  isUpdate = false
+  modelName = ''
+  clientId = ''
+  table = new TableStore({
+    request: (params) => {
+      return customerRatApi.postClientPage(params)
+    },
+  })
+  createRat = true
+  postClientInfo = async(clientId, model, cb) => {
+    try {
+      const res = await customerRatApi.postClientInfo({ clientId })
+      const fieldsValues = this.isUpdate && model ? {...res,model} : {...res}
+      this.showDistrict = res.hymxFlag && !res.province
+      this.createModal.getFormStore().setFieldsValue(fieldsValues)
+      this.createRat = true
+    } catch (error) {
+      this.createRat = false
+      cb && cb()
+    }
+  }
+  createModal = new ModalStore({
+    onOpen: (record) => {
+      this.clientDisabled = true
+      record && this.postClientInfo(record?.clientId, record?.modelCode)
+    },
+    onFinish: async (params) => {
+      if(!this.createRat){
+        message.info('客户基本信息不完整，请在客户管理模块维护客户基本信息后再发起评级！')
+        return
+      }
+      const initial = this.createModal.getInitialValues()
+      const isEdit = initial?.editType === 'edit'
+      if (isEdit) {
+        await customerRatApi.postClientUpdate({
+          id: initial?.id,
+          [(!params.clientCode && params.model.value === 'client_hymx') ? 'clientName' : 'clientId']: initial?.clientId,
+          code: params.model.value,
+          name: params.model.label,
+        })
+        message.success('更新成功')
+        this.createModal.close()
+        this.table.search()
+        return
+      }
+      const { model, ...rest } = params
+      let _rest = rest
+      if(model.value === 'client_hymx' && _rest.clientId && !params.clientCode){
+        _rest = { ..._rest, clientName:rest.clientId }
+        delete _rest.clientId
+      }else if(this.clientId){
+        _rest = { ..._rest, clientId:this.clientId }
+      }
+      const { exist } = await customerRatApi.postClientAccessCheck({
+        ..._rest,
+        code: model.value || model,
+        name: model.label || this.modelName,
+      })
+      const add = async () => {
+        const { id } = await customerRatApi.postClientAdd({
+          ..._rest,
+          code: model.value || model,
+          name: model.label || this.modelName,
+        })
+        this.createModal.close()
+        this.table.search()
+        history.push(`/customer/customerRat/detail/${id}?model=${model.value}`)
+      }
+      if (exist) {
+        Modal.confirm({
+          title: '提示',
+          content: `当前客户存在正在生效的评级，是否对当前评级进行更新？`,
+          okText: '更新',
+          onOk: async () => {
+            await add()
+          },
+          cancelText: '不更新',
+          onCancel: () => {
+            this.createModal.close()
+          },
+        })
+      } else {
+        await add()
+      }
+    },
+  })
+  delete = async (id) => {
+    await customerRatApi.postClientDelete({ id })
+    this.table.search()
+    message.success('删除成功')
+  }
+  add = () => {
+    this.isUpdate = false
+    this.clientDisabled = true
+    this.createModal.open()
+  }
+  edit = (record) => {
+    this.isUpdate = true
+    this.modelName = record.modelName
+    this.clientId = record.clientId
+    this.createModal.open({ editType: 'edit', ...record, model:record.modelCode })
+  }
+  recordModal = new ModalStore({})
+  recordTable = new TableStore({
+    request: async (params) => {
+      const res = await customerRatApi.postClientOverturnRecord(params)
+      return res
+    },
+  })
+  view = (id) => {
+    this.recordModal.open({ id })
+    setTimeout(() => {
+      this.recordTable.search({ id })
+    }, 10)
+  }
+}
+export default new Store()
