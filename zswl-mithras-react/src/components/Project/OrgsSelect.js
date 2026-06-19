1707bdd8fe6c7c21675a8e0c30927d5e8e97ae7e
Chun <caochun@gmail.com>
2026-06-19 09:54:12 +0800

    refactor: move project select api to api layer

--- zswl-mithras-react/src/components/Project/OrgsSelect.js
+++ zswl-mithras-react/src/components/Project/OrgsSelect.js
@@ -1,7 +1,7 @@
 import { Select } from '@zswl/components'
 import { useEffect, useState } from 'react'
 import { debounce as _debounce } from 'lodash'
-import Api from './projectSelectApi'
+import Api from '@/api/project/projectSelectApi'
 
 const OrgsSelect = ({ queryParams, referer, ...params }) => {
   const [OrgsList, setOrgsList] = useState()
--- zswl-mithras-react/src/components/Project/ProjectSelect.js
+++ zswl-mithras-react/src/components/Project/ProjectSelect.js
@@ -1,7 +1,7 @@
 import { Select } from '@zswl/components'
 import { forwardRef, useEffect, useImperativeHandle, useState } from 'react'
 import { debounce as _debounce } from 'lodash'
-import Api from './projectSelectApi'
+import Api from '@/api/project/projectSelectApi'
 
 const ProjectSelect = ({ queryParams, referer, ...params }, ref) => {
   const [projectList, setProjectList] = useState()
