427b15dd449cf06fb59501e5a0a43cd4489dfd70
Chun <caochun@gmail.com>
2026-06-20 10:49:13 +0800

    refactor: move cpm payment write off into domain

---
+++ zswl-mithras-react/src/components/Cpm/PaymentWriteOff/FundConfirm.js
@@ -0,0 +1,25 @@
+import { Modal, Form } from '@zswl/components'
+import { DatePicker } from 'antd'
+import Amount from '@/components/Amount'
+import CpmAmountNumber from '../AmountNumber'
+import { rules } from '@/utils'
+
+const { Item } = Form
+
+const Index = ({ store }) => {
+  return (
+    <Modal title={'创建合同'} store={store.fundConfirmModal} okText={'确定'} destroyOnClose>
+      <Form labelCol={{ span: 6 }} preserve={false}>
+        <Item name="amount" label="交易金额" rules={[rules.required()]}>
+          <Amount>
+            <CpmAmountNumber />
+          </Amount>
+        </Item>
+        <Item label={'评估日期'} name={'assessDate'} rules={[rules.required()]}>
+          <DatePicker style={{ width: '100%' }} format="yyyy-MM-DD" />
+        </Item>
+      </Form>
+    </Modal>
+  )
+}
+export default Index
